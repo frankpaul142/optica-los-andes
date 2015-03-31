cf6f057673fddd8009cc4222a06dbd3f17359c72
Rodney Ledesma <rodney.ledesma@share.com.ec>
2015-03-31 16:19:15 -0500

    transiciones angular prueba

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,6 @@
 /* @var $this yii\web\View */
 $this->title = 'OPTICA LOS ANDES | Lentes, Gafas, Lunas';
 ?>
-<section id="cont-home" ng-view>
+<section id="cont-home" class="page {{ pageClass }}" ng-view>
 
 </section>
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -1,21 +1,26 @@
 var controllers = angular.module('OpticaControllers', []);
 
-controllers.controller('HomeCtrl', function(){
+controllers.controller('HomeCtrl', function($scope){
 	console.log('HomeCtrl');
+	$scope.pageClass = 'page-home';
 });
 
-controllers.controller('PromoCtrl', function(){
+controllers.controller('PromoCtrl', function($scope){
 	console.log('PromoCtrl');
+	$scope.pageClass = 'page-about';
 });
 
-controllers.controller('ContactoCtrl', function(){
+controllers.controller('ContactoCtrl', function($scope){
 	console.log('ContactoCtrl');
+	$scope.pageClass = 'page-contact';
 });
 
-controllers.controller('QuienesCtrl', function(){
+controllers.controller('QuienesCtrl', function($scope){
 	console.log('QuienesCtrl');
+	$scope.pageClass = 'page-about';
 });
 
-controllers.controller('TrabajaCtrl', function(){
+controllers.controller('TrabajaCtrl', function($scope){
 	console.log('TrabajaCtrl');
+	$scope.pageClass = 'page-contact';
 });
