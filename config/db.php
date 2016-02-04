3a3285a59226d30d8fbdb7cefc338a04609036ce
Franklin Paula <frankpaul142@gmail.com>
2016-02-04 10:14:46 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=166.63.0.204;dbname=opticala_web',
-    'username' => 'opticala_admin',
-    'password' => '+NdnL(fv6VQT',
+    'dsn' => 'mysql:host=localhost;dbname=opticala_web',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
